b27709bfb9e1cba8e8114935e70068ed4a3381b7
varenyamBakshi <varenyam101@gmail.com>
2020-07-09 17:46:22 +0530

    adding additional apps

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+app_name = 'blog'
+urlpatterns = [
+    path('', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(),name='post-create'),
+    path('about/', views.about, name='about'),
+]
