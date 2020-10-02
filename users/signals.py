f1afdf517fc35f6bc7e4045ee956072bb5c5e845
Varenyam Bakshi <56773731+varenyamBakshi@users.noreply.github.com>
2020-10-02 12:43:19 +0530

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,9 @@
+#line 6
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+#make it "from .models import Profile"
+import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
