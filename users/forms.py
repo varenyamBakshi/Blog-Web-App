8900d9c9162dab24f2827b8cc6ea6f98b643af52
Varenyam Bakshi <56773731+varenyamBakshi@users.noreply.github.com>
2020-10-02 12:46:30 +0530

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
+#line 9
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    # set email equal to 'forms.EmailField()'
+    email = Pass
 
     class Meta:
         model = User
